e34a0c68fcf6b150fef277d9bd9e25a45c17e3d2
Gabriel Gregorio <gabriel.gregorio1987@gmail.com>
2020-08-05 16:03:54 -0300

    Regas UsuarioBean

--- Sorveteria_Online/src/main/java/br/com/sorveteria/controller/UsuarioBean.java
+++ Sorveteria_Online/src/main/java/br/com/sorveteria/controller/UsuarioBean.java
@@ -3,8 +3,12 @@ package br.com.sorveteria.controller;
 import java.io.Serializable;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpSession;
+
 import br.com.sorveteria.CRUD.UsuarioCRUD;
 import br.com.sorveteria.DAO.UsuarioDAO;
+import br.com.sorveteria.Util.Faces;
 import br.com.sorveteria.entity.Usuario;
 
 @ManagedBean
@@ -16,32 +20,71 @@ public class UsuarioBean implements Serializable {
 	private String txtLogin;
 	private String txtSenha;
 	private UsuarioCRUD<Usuario> usuarioDAO;
+	private Faces faces;
 
 	public UsuarioBean() {
 		this.usuario = new Usuario();
 		this.usuarioDAO = new UsuarioDAO();
+		this.faces = new Faces();
 
 	}
 
 	public void salvar() {
-		Usuario user = new Usuario(this.usuario.getNome(), this.usuario.getLogin(), this.usuario.getSenha());
-		this.usuarioDAO.salvar(user);
+		boolean verificar = false;
+		if (this.usuario.getNome().isEmpty() || this.usuario.getLogin().isEmpty()
+				|| this.usuario.getSenha().isEmpty()) {
+			this.faces.msgError("Preencha os Campos");
+		} else {
+			Usuario novoUser = new Usuario(this.usuario.getNome(), this.usuario.getLogin(), this.usuario.getSenha());
+			for (Usuario user : this.usuarioDAO.listar()) {
+				if (this.usuario.getLogin().equals(user.getLogin())) {
+					verificar = true;
+				}
+			}
+			if (verificar) {
+				this.faces.msgError("você já possui cadastro!");
+			} else {
+				this.usuarioDAO.salvar(novoUser);
+				this.faces.msgInfor("cadastrado com sucesso!");
+			}
+		}
 	}
 
 	public String entrar() {
-		boolean verificar = false;
-		for (Usuario user : this.usuarioDAO.listar()) {
-			if (user.getLogin().equals(this.txtLogin) && user.getSenha().equals(this.txtSenha)) {
-				verificar = true;
+
+		Usuario logIn = null;
+		if (this.txtLogin.isEmpty() || this.txtSenha.isEmpty()) {
+			this.faces.msgError("Preencha os Campos!");
+		} else {
+
+			for (Usuario user : this.usuarioDAO.listar()) {
+				if (user.getLogin().equals(this.txtLogin) && user.getSenha().equals(this.txtSenha)) {
+					logIn = user;
+				}
+			}
+
+			if (logIn == null && !this.txtLogin.isEmpty() && !this.txtSenha.isEmpty()) {
+				this.txtLogin = null;
+				this.txtSenha = null;
+				this.faces.msgError("Login ou senha incorretos");
+
+			} else {
+				HttpSession sessao = (HttpSession) FacesContext.getCurrentInstance().getExternalContext()
+						.getSession(true);
+				sessao.setAttribute("logIn", logIn);
+				return "/pagina/pedido.xhtml?faces-redirect=true&amp;includeViewParams=true";
 			}
 		}
 
-		if (verificar) {
-			System.out.println("entrou");
-		} else {
-			System.out.println("login ou senha incorretos");
-		}
 		return "";
+	}
+
+	public Faces getFaces() {
+		return faces;
+	}
+
+	public void setFaces(Faces faces) {
+		this.faces = faces;
 	}
 
 	public Usuario getUsuario() {
